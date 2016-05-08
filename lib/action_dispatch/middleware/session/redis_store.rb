a24a5bd168b99406a3cee9bdb5cf0a61c13d291c
Ryan McGary <ryan.mcgary@gmail.com>
2016-05-07 19:16:49 -0500

    fix for domain :all issue

--- lib/action_dispatch/middleware/session/redis_store.rb
+++ lib/action_dispatch/middleware/session/redis_store.rb
@@ -7,11 +7,24 @@ module ActionDispatch
     class RedisStore < Rack::Session::Redis
       include Compatibility
       include StaleSessionCheck
+      include SessionObject
+
       def initialize(app, options = {})
         options = options.dup
         options[:redis_server] ||= options[:servers]
         super
       end
+
+      private
+
+      def set_cookie(env, session_id, cookie)
+
+        if env.is_a? ActionDispatch::Request
+          request = env
+        else
+          request = ActionDispatch::Request.new(env)
+        end
+        request.cookie_jar[key] = cookie
+      end
     end
   end
-end
